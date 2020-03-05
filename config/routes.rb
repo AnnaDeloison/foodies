866cc072277e3a62e22179e3a71d1289e410e068
Nicolas Bouygues <bouygues.nicolas@gmail.com>
2020-03-05 17:45:01 +0100

    ajouté le delete au cart

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   get 'order_confirmation/:order_id', to: 'orders#success', as: :success
   get 'recipes/:id/add_cart', to: 'recipes#add_cart', as: :add_cart
   get 'show_cart', to: 'recipes#show_cart', as: :show_cart
+  get 'show_cart/delete_recipe', to: 'recipes#destroy_cart', as: :delete_recipe
   mount StripeEvent::Engine, at: '/stripe-webhooks'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
