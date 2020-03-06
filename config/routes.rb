7a11dd39179ccbeb9f39790f688534c231174175
Nicolas Bouygues <bouygues.nicolas@gmail.com>
2020-03-06 16:22:28 +0100

    added user page with history of commands

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   get 'recipes/:id/add_cart', to: 'recipes#add_cart', as: :add_cart
   get 'show_cart', to: 'recipes#show_cart', as: :show_cart
   get 'show_cart/delete_recipe', to: 'recipes#destroy_cart', as: :delete_recipe
+  get 'profile', to: 'users#show', as: :profile
   mount StripeEvent::Engine, at: '/stripe-webhooks'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
